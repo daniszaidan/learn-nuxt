8043b13e623bdc6afc37f2784af79fa839f0f92b
Danis Zaidan <daniszaidan@gmail.com>
2025-07-08 02:10:58 +0700

    fix hover

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="space-y-5">
-    <h1
-      class="transition-all duration-200 text-red-500 hover:hidden hover:text-blue-300"
-    >
-      Blog Post App
-    </h1>
-
-    <button class="bg-sky-500 hover:bg-sky-700">Save changes</button>
+    <h1>Blog Post App</h1>
 
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3">
       <Card v-for="post in posts" :key="post?.id" :post="post" />
